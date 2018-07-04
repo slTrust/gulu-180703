f2c767372af56313848fb803e3bcb7d37b62ef99
huangjx <trustfor@sina.cn>
2018-07-04 23:02:45 +0800

    使用 chai.expect 添加四个测试用例，第五个不会

--- src/app.js
+++ src/app.js
@@ -15,3 +15,103 @@ new Vue({
         loading3:false
     }
 })
+
+import chai from 'chai'
+const expect = chai.expect
+
+// 单元测试
+
+// 测试 setting
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-setting')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+// 测试 loading
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+// 测试 order
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+// 测试 设置了icon-positon 后的 order
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true,
+            iconPosition:'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+// 测试 click
+{
+    // mock
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData:{
+            icon:'setting',
+            loading:true,
+            iconPosition:'right'
+        }
+    })
+    vm.$mount()
+
+    vm.$on('click',function(){
+        console.log(1)
+    })
+    // 希望这个函数被执行
+    let button = vm.$el
+    button.click()
+}
